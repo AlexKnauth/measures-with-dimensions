9e26009d19068d3d8cf56264b3c298e1b4f6c518
AlexKnauth <alexander@knauth.org>
2014-10-09 09:42:06 -0400

    add deps

--- info.rkt
+++ info.rkt
@@ -2,3 +2,19 @@
 
 (define collection 'multi)
 
+(define deps '("base"
+               "typed-racket-lib"
+               "typed-racket-more"
+               "rackjure"
+               "math-lib"
+               "plot-lib"
+               "htdp-lib"
+               ))
+
+(define build-deps '("rackunit-lib"
+                     "scribble-lib"
+                     "sandbox-lib"
+                     "racket-doc"
+                     "typed-racket-doc"
+                     ))
+
