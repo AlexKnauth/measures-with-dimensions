b1ddb46f69338a37d34105d10ed1c55db1d3475c
AlexKnauth <alexander@knauth.org>
2014-10-31 18:37:15 -0400

    get m* working for typed racket and update docs cont.

---
+++ typed/measures-with-dimensions/typed-operations.rkt
@@ -0,0 +1,13 @@
+#lang typed/racket
+
+(provide (all-from-out "untyped-operations.rkt"))
+
+(require "untyped-operations.rkt"
+         "untyped-utils.rkt")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(untyped-module*
+ )
+
+
