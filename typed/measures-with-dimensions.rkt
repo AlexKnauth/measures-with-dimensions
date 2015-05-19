350ec1509f4b3a336a2210ac640450f43d9b8f93
AlexKnauth <alexander@knauth.org>
2015-05-19 15:16:11 -0400

    stuff

--- typed/measures-with-dimensions.rkt
+++ typed/measures-with-dimensions.rkt
@@ -24,20 +24,35 @@
          "measures-with-dimensions/measure-types.rkt"
          "measures-with-dimensions/typed-operations.rkt"
          "measures-with-dimensions/temperature.rkt"
-         "measures-with-dimensions/untyped-utils.rkt"
          )
 
-(untyped-module*
- [#:all-from (submod "measures-with-dimensions/dimension-struct.rkt" untyped)
-             (submod "measures-with-dimensions/dimension-operations.rkt" untyped)
-             (submod "measures-with-dimensions/dimensions.rkt" untyped)
-             (submod "measures-with-dimensions/unit-struct.rkt" untyped)
-             (submod "measures-with-dimensions/unit-operations.rkt" untyped)
-             (submod "measures-with-dimensions/units.rkt" untyped)
-             (submod "measures-with-dimensions/measure-struct.rkt" untyped)
-             (submod "measures-with-dimensions/physical-constants.rkt" untyped)
-             (submod "measures-with-dimensions/measure-types.rkt" untyped)
-             (submod "measures-with-dimensions/typed-operations.rkt" untyped)
-             (submod "measures-with-dimensions/temperature.rkt" untyped)
-             "measures-with-dimensions/untyped-operations.rkt"
-             ])
+(module* untyped racket/base
+  (provide (all-from-out
+            (submod "measures-with-dimensions/dimension-struct.rkt" untyped)
+            (submod "measures-with-dimensions/dimension-operations.rkt" untyped)
+            (submod "measures-with-dimensions/dimensions.rkt" untyped)
+            (submod "measures-with-dimensions/unit-struct.rkt" untyped)
+            (submod "measures-with-dimensions/unit-operations.rkt" untyped)
+            (submod "measures-with-dimensions/units.rkt" untyped)
+            (submod "measures-with-dimensions/measure-struct.rkt" untyped)
+            (submod "measures-with-dimensions/physical-constants.rkt" untyped)
+            (submod "measures-with-dimensions/measure-types.rkt" untyped)
+            (submod "measures-with-dimensions/typed-operations.rkt" untyped)
+            (submod "measures-with-dimensions/temperature.rkt" untyped)
+            "measures-with-dimensions/untyped-operations.rkt"
+            ))
+  
+  (require (submod "measures-with-dimensions/dimension-struct.rkt" untyped)
+           (submod "measures-with-dimensions/dimension-operations.rkt" untyped)
+           (submod "measures-with-dimensions/dimensions.rkt" untyped)
+           (submod "measures-with-dimensions/unit-struct.rkt" untyped)
+           (submod "measures-with-dimensions/unit-operations.rkt" untyped)
+           (submod "measures-with-dimensions/units.rkt" untyped)
+           (submod "measures-with-dimensions/measure-struct.rkt" untyped)
+           (submod "measures-with-dimensions/physical-constants.rkt" untyped)
+           (submod "measures-with-dimensions/measure-types.rkt" untyped)
+           (submod "measures-with-dimensions/typed-operations.rkt" untyped)
+           (submod "measures-with-dimensions/temperature.rkt" untyped)
+           "measures-with-dimensions/untyped-operations.rkt"
+           )
+  )
