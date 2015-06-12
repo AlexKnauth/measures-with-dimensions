f5e3712ece4267968b67ca91b2db4494ba3e6e94
AlexKnauth <alexander@knauth.org>
2015-06-12 12:39:15 -0400

    more abs paths

--- measures-with-dimensions/chemistry.rkt
+++ measures-with-dimensions/chemistry.rkt
@@ -1,3 +1,3 @@
 #lang racket/base
-(require (submod "../typed/measures-with-dimensions/chemistry.rkt" untyped))
-(provide (all-from-out (submod "../typed/measures-with-dimensions/chemistry.rkt" untyped)))
+(require (submod typed/measures-with-dimensions/chemistry untyped))
+(provide (all-from-out (submod typed/measures-with-dimensions/chemistry untyped)))
--- measures-with-dimensions/temperature.rkt
+++ measures-with-dimensions/temperature.rkt
@@ -1,3 +1,3 @@
 #lang racket/base
-(require (submod "../typed/measures-with-dimensions/temperature.rkt" untyped))
-(provide (all-from-out (submod "../typed/measures-with-dimensions/temperature.rkt" untyped)))
+(require (submod typed/measures-with-dimensions/temperature untyped))
+(provide (all-from-out (submod typed/measures-with-dimensions/temperature untyped)))
