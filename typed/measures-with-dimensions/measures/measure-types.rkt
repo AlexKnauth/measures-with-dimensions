43cfa5b5c5408a2ac00a41ce0d615c37dede58d6
AlexKnauth <alexander@knauth.org>
2015-11-08 14:45:07 -0500

    more sweet-exp

--- typed/measures-with-dimensions/measures/measure-types.rkt
+++ typed/measures-with-dimensions/measures/measure-types.rkt
@@ -20,153 +20,153 @@ require syntax/parse/define
     [id:id num-ty:expr] ...)
   (defmulti-types [id (Measureof num-ty unit-ty)] ...))
 
-(defmulti-types/Measureof Dimensionless-Unit
+defmulti-types/Measureof Dimensionless-Unit
   [Dimensionless Num/Vec]
   [Dimensionless-Number Number]
   [Dimensionless-Real Real]
-  [Dimensionless-Vector Vec])
+  [Dimensionless-Vector Vec]
 (define-type Dimensionless-Mesarue Dimensionless)
 
-(defmulti-types/Measureof Mass-Unit
+defmulti-types/Measureof Mass-Unit
   [Mass Nonnegative-Real]
   [Mass-Real-Measure Real]
-  [Mass-Measure Num/Vec])
-(defmulti-types/Measureof Length-Unit
+  [Mass-Measure Num/Vec]
+defmulti-types/Measureof Length-Unit
   [Position Real/Vec]
   [Length Nonnegative-Real]
   [Position-Vector Vec]
   [Length-Real-Measure Real]
-  [Length-Measure Num/Vec])
-(defmulti-types/Measureof Time-Unit
+  [Length-Measure Num/Vec]
+defmulti-types/Measureof Time-Unit
   [Time Real]
-  [Time-Measure Num/Vec])
-(defmulti-types/Measureof Charge-Unit
+  [Time-Measure Num/Vec]
+defmulti-types/Measureof Charge-Unit
   [Charge Real]
-  [Charge-Measure Num/Vec])
-(defmulti-types/Measureof Absolute-Temperature-Unit
+  [Charge-Measure Num/Vec]
+defmulti-types/Measureof Absolute-Temperature-Unit
   [Absolute-Temperature Nonnegative-Real]
   [Temperature-Real-Measure Real]
-  [Temperature-Measure Num/Vec])
+  [Temperature-Measure Num/Vec]
 
-(defmulti-types/Measureof Area-Unit
+defmulti-types/Measureof Area-Unit
   [Area Nonnegative-Real]
   [Area-Real-Measure Real]
-  [Area-Measure Num/Vec])
-(defmulti-types/Measureof Volume-Unit
+  [Area-Measure Num/Vec]
+defmulti-types/Measureof Volume-Unit
   [Volume Nonnegative-Real]
   [Volume-Real-Measure Real]
-  [Volume-Measure Num/Vec])
+  [Volume-Measure Num/Vec]
 
-(defmulti-types/Measureof Mass-Density-Unit
+defmulti-types/Measureof Mass-Density-Unit
   [Mass-Density Nonnegative-Real]
   [Mass-Density-Real-Measure Real]
-  [Mass-Density-Measure Num/Vec])
-(defmulti-types/Measureof Charge-Density-Unit
+  [Mass-Density-Measure Num/Vec]
+defmulti-types/Measureof Charge-Density-Unit
   [Charge-Density Real]
-  [Change-Density-Measure Num/Vec])
+  [Change-Density-Measure Num/Vec]
 
-(defmulti-types/Measureof Velocity-Unit
+defmulti-types/Measureof Velocity-Unit
   [Velocity Real/Vec]
   [Speed Nonnegative-Real]
   [Velocity-Vector Vec]
   [Velocity-Real-Measure Real]
-  [Velocity-Measure Num/Vec])
-(defmulti-types/Measureof Acceleration-Unit
+  [Velocity-Measure Num/Vec]
+defmulti-types/Measureof Acceleration-Unit
   [Acceleration Real/Vec]
   [Acceleration-Real-Measure Real]
   [Acceleration-Vector Vec]
-  [Acceleration-Measure Num/Vec])
-(defmulti-types/Measureof Force-Unit
+  [Acceleration-Measure Num/Vec]
+defmulti-types/Measureof Force-Unit
   [Force Real/Vec]
   [Force-Vector Vec]
   [Force-Real-Measure Real]
-  [Force-Measure Num/Vec])
-(defmulti-types/Measureof Momentum-Unit
+  [Force-Measure Num/Vec]
+defmulti-types/Measureof Momentum-Unit
   [Momentum Real/Vec]
   [Momentum-Vector Vec]
   [Momentum-Real-Measure Real]
-  [Momentum-Measure Num/Vec])
+  [Momentum-Measure Num/Vec]
 
-(defmulti-types/Measureof Energy-Unit
+defmulti-types/Measureof Energy-Unit
   [Energy Real]
   [Work Real]
-  [Energy/Work/Torqe-Measure Num/Vec])
-(defmulti-types/Measureof Torque-Unit
+  [Energy/Work/Torqe-Measure Num/Vec]
+defmulti-types/Measureof Torque-Unit
   [Torque Real/Vec]
-  [Torque-Vector Vec])
-(defmulti-types/Measureof Power-Unit
+  [Torque-Vector Vec]
+defmulti-types/Measureof Power-Unit
   [Power Real]
-  [Power-Measure Num/Vec])
+  [Power-Measure Num/Vec]
 
-(defmulti-types/Measureof Pressure-Unit
+defmulti-types/Measureof Pressure-Unit
   [Pressure Nonnegative-Real]
   [Pressure-Real-Measure Real]
-  [Pressure-Measure Num/Vec])
+  [Pressure-Measure Num/Vec]
 
-(defmulti-types/Measureof Entropy-Unit
+defmulti-types/Measureof Entropy-Unit
   [Entropy Real]
   [Heat-Capacity Nonnegative-Real]
   [Molar-Specific-Heat Nonnegative-Real]
-  [Entropy/Heat-Capacity/Molar-Specific-Heat-Measure Num/Vec])
-(defmulti-types/Measureof Specific-Heat-Unit
+  [Entropy/Heat-Capacity/Molar-Specific-Heat-Measure Num/Vec]
+defmulti-types/Measureof Specific-Heat-Unit
   [Specific-Heat Nonnegative-Real]
   [Specific-Heat-Real-Measure Real]
-  [Specific-Heat-Measure Num/Vec])
+  [Specific-Heat-Measure Num/Vec]
 
-(defmulti-types/Measureof Electric-Field-Unit
+defmulti-types/Measureof Electric-Field-Unit
   [Electric-Field Real/Vec]
   [Electric-Field-Vector Vec]
   [Electric-Field-Real-Measure Real]
-  [Electric-Field-Measure Num/Vec])
-(defmulti-types/Measureof Electric-Potential-Unit
+  [Electric-Field-Measure Num/Vec]
+defmulti-types/Measureof Electric-Potential-Unit
   [Electric-Potential Real]
-  [Electric-Potential-Measure Num/Vec])
-(defmulti-types
+  [Electric-Potential-Measure Num/Vec]
+defmulti-types
   [Voltage Electric-Potential]
-  [Emf Voltage])
-(defmulti-types/Measureof Capacitance-Unit
+  [Emf Voltage]
+defmulti-types/Measureof Capacitance-Unit
   [Capacitance Nonnegative-Real]
   [Capacitance-Real-Measure Real]
-  [Capacitance-Measure Num/Vec])
+  [Capacitance-Measure Num/Vec]
 
-(defmulti-types/Measureof Current-Unit
+defmulti-types/Measureof Current-Unit
   [Current Real]
-  [Current-Measure Num/Vec])
-(defmulti-types/Measureof Current-Density-Unit
+  [Current-Measure Num/Vec]
+defmulti-types/Measureof Current-Density-Unit
   [Current-Density Real/Vec]
   [Current-Density-Vector Vec]
   [Current-Density-Real-Measure Real]
-  [Current-Density-Measure Num/Vec])
-(defmulti-types/Measureof Resistance-Unit
+  [Current-Density-Measure Num/Vec]
+defmulti-types/Measureof Resistance-Unit
   [Resistance Nonnegative-Real]
   [Resistance-Real-Measure Real]
-  [Resistance-Measure Num/Vec])
-(defmulti-types/Measureof Resistivity-Unit
+  [Resistance-Measure Num/Vec]
+defmulti-types/Measureof Resistivity-Unit
   [Resistivity Nonnegative-Real]
   [Resistivity-Real-Measure Real]
-  [Resistivity-Measure Num/Vec])
-(defmulti-types/Measureof Conductivity-Unit
+  [Resistivity-Measure Num/Vec]
+defmulti-types/Measureof Conductivity-Unit
   [Conductivity Nonnegative-Real]
   [Conductivity-Real-Measure Real]
-  [Conductivity-Measure Num/Vec])
+  [Conductivity-Measure Num/Vec]
 
-(defmulti-types/Measureof Magnetic-Field-Unit
+defmulti-types/Measureof Magnetic-Field-Unit
   [Magnetic-Field Real/Vec]
   [Magnetic-Field-Vector Vec]
   [Magnetic-Field-Real-Measure Real]
-  [Magnetic-Field-Measure Num/Vec])
+  [Magnetic-Field-Measure Num/Vec]
 
-(defmulti-types/Measureof Electric-Flux-Unit
+defmulti-types/Measureof Electric-Flux-Unit
   [Electric-Flux Real]
-  [Electric-Flux-Measure Num/Vec])
-(defmulti-types/Measureof Magnetic-Flux-Unit
+  [Electric-Flux-Measure Num/Vec]
+defmulti-types/Measureof Magnetic-Flux-Unit
   [Magnetic-Flux Real]
-  [Magnetic-Flux-Measure Num/Vec])
+  [Magnetic-Flux-Measure Num/Vec]
 
-(defmulti-types/Measureof Inductance-Unit
+defmulti-types/Measureof Inductance-Unit
   [Inductance Nonnegative-Real]
   [Inductance-Real-Measure Real]
-  [Inductance-Measure Num/Vec])
+  [Inductance-Measure Num/Vec]
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
--- typed/measures-with-dimensions/units/units.rkt
+++ typed/measures-with-dimensions/units/units.rkt
@@ -164,7 +164,7 @@ require syntax/parse/define
   (begin (define-unit id : Unit-Type expr) ...))
 
 ;; Dimensionless-Units:
-(define-units/type Dimensionless-Unit
+define-units/type Dimensionless-Unit
   [thing (make-Unit 'thing 1 dimensionless-dimension)]
   [mol (make-Unit 'mol (cast (* #i6.02214129 (10^ 23)) Positive-Real) dimensionless-dimension)]
   [radian 1-unit]
@@ -175,11 +175,11 @@ require syntax/parse/define
   [pi-unit (u/ 2pi-unit 2)]
   [degree (u/ turn 360)]
   [percent (u/ 1-unit 100)]
-  )
+
 (define ° : Dimensionless-Unit degree)
 
 ;; Mass:
-(define-units/type Mass-Unit
+define-units/type Mass-Unit
   [kilogram (make-Unit 'kilogram 1 mass-dimension)]
   [gram (make-Unit 'gram 1/1000 mass-dimension)]
   [pound-mass (u* #e0.45359237 kilogram)]
@@ -187,10 +187,10 @@ require syntax/parse/define
   [ton (u* 2000 pound-mass)]
   [atomic-mass-unit (u* #i1.6605402e-27 kilogram)]
   [planck-mass (u* #i2.17651e-8 kilogram)]
-  )
+
 
 ;; Length:
-(define-units/type Length-Unit
+define-units/type Length-Unit
   [meter (make-Unit 'meter 1 length-dimension)]
   [centimeter (centi meter)]
   [millimeter (milli meter)]
@@ -205,10 +205,10 @@ require syntax/parse/define
   [light-year (u* 9.4607304725808e15 meter)]
   [nautical-mile (u* 1852 meter)]
   [planck-length (u* #i1.616199e-35 meter)]
-  )
+
 
 ;; Time:
-(define-units/type Time-Unit
+define-units/type Time-Unit
   [second (make-Unit 'second 1 time-dimension)]
   [minute (u* 60 second)]
   [hour (u* 60 minute)]
@@ -224,27 +224,27 @@ require syntax/parse/define
   [microsecond (micro second)]
   [nanosecond (nano second)]
   [planck-time (u* #i5.39106e-44 second)]
-  )
+
 
 ;; Charge:
-(define-units/type Charge-Unit
+define-units/type Charge-Unit
   [coulomb (make-Unit 'coulomb 1 charge-dimension)]
   [microcoulomb (micro coulomb)]
   [nanocoulomb (nano coulomb)]
   [picocoulomb (pico coulomb)]
   [elementary-charge-unit (u* #i1.602176565e-19 coulomb)]
   [planck-charge (u* #i1.875545956e-18 coulomb)]
-  )
+
 
 ;; Absolute Temperature:
-(define-units/type Absolute-Temperature-Unit
+define-units/type Absolute-Temperature-Unit
   [kelvin (make-Unit 'kelvin 1 temperature-dimension)]
   [rankine (u* 5/9 kelvin)]
   [planck-temperature (u* #i1.416833e32 kelvin)]
-  )
+
 
 ;; Area:
-(define-units/type Area-Unit
+define-units/type Area-Unit
   [square-meter (usqr meter)]
   [square-centimeter (usqr centimeter)]
   [square-foot (usqr foot)]
@@ -252,10 +252,10 @@ require syntax/parse/define
   [square-mile (usqr mile)]
   [acre (u* 43560 square-foot)]
   [planck-area (usqr planck-length)]
-  )
+
 
 ;; Volume:
-(define-units/type Volume-Unit
+define-units/type Volume-Unit
   [cubic-meter (uexpt meter 3)]
   [cubic-centimeter (uexpt centimeter 3)]
   [liter (uexpt decimeter 3)]
@@ -270,43 +270,43 @@ require syntax/parse/define
   [tablespoon (u/ fluid-ounce 2)]
   [teaspoon (u/ tablespoon 3)]
   [planck-volume (uexpt planck-length 3)]
-  )
+
 
 ;; Velocity/Speed:
-(define-units/type Velocity-Unit
+define-units/type Velocity-Unit
   [meter-per-second (make-Unit 'meter-per-second 1 velocity-dimension)]
   [mile-per-hour (u/ mile hour)]
   [foot-per-second (u/ foot second)]
   [speed-of-light-unit (u* 299792458 meter-per-second)]
   [knot (u/ nautical-mile hour)]
   [planck-velocity speed-of-light-unit]
-  )
+
 
 ;; Acceleration
-(define-units/type Acceleration-Unit
+define-units/type Acceleration-Unit
   [meter-per-second-squared (make-Unit 'meter-per-second-squared 1 acceleration-dimension)]
   [gravitational-acceleration-unit (u* #e9.80665 meter-per-second-squared)]
-  )
+
 
 ;; Force:
-(define-units/type Force-Unit
+define-units/type Force-Unit
   [newton (make-Unit 'newton 1 force-dimension)]
   [pound-force (u* gravitational-acceleration-unit pound-mass)]
-  )
+
 
 ;; Momentum:
-(define-units/type Momentum-Unit
-  )
+define-units/type Momentum-Unit
+
 
 ;; Power:
-(define-units/type Power-Unit
+define-units/type Power-Unit
   [watt (make-Unit 'watt 1 power-dimension)]
   [kilowatt (kilo watt)]
   [horsepower (u* #e746 watt)]
-  )
+
 
 ;; Energy/Work/Torque:
-(define-units/type Energy-Unit
+define-units/type Energy-Unit
   [joule (make-Unit 'joule 1 energy-dimension)]
   [newton-meter (u* newton meter)]
   [calorie (u* #e4.184 joule)]
@@ -314,10 +314,10 @@ require syntax/parse/define
   [british-thermal-unit (u* #e1055.05585262 joule)]
   [kilowatt-hour (u* kilowatt hour)]
   [electron-volt (u* elementary-charge-unit voltage-dimension)]
-  )
+
 
 ;; Pressure:
-(define-units/type Pressure-Unit
+define-units/type Pressure-Unit
   [pascal (make-Unit 'pascal 1 pressure-dimension)]
   [kilopascal (kilo pascal)]
   [pound-per-square-inch (u/ pound-force square-inch)]
@@ -326,61 +326,61 @@ require syntax/parse/define
   [millibar (milli bar)]
   [torr (u/ atmosphere #e760)]
   [millimeter-of-mercury (u* #e13.5951 #e9.80665 pascal)]
-  )
+
 
 ;; Entropy, Energy per Temperature, and Gas Constants:
-(define-units/type Entropy-Unit
+define-units/type Entropy-Unit
   [ideal-gas-constant-unit (u* #i8.3144621 (u/ (u* pascal cubic-meter) (u* kelvin mol)))]
   [bolzmann-constant-unit (u* #i1.3806488e-23 (u/ joule kelvin))]
-  )
+
 
 ;; Electric-Field:
-(define-units/type Electric-Field-Unit
-  )
+define-units/type Electric-Field-Unit
+
 
 ;; Voltage/Emf/Electric-Potential:
-(define-units/type Voltage-Unit
+define-units/type Voltage-Unit
   [volt (make-Unit 'volt 1 voltage-dimension)]
-  )
+
 
 ;; Capacitance:
-(define-units/type Capacitance-Unit
+define-units/type Capacitance-Unit
   [farrad (make-Unit 'farrad 1 capacitance-dimension)]
-  )
+
 
 ;; Current:
-(define-units/type Current-Unit
+define-units/type Current-Unit
   [ampere (make-Unit 'ampere 1 current-dimension)]
-  )
+
 
 ;; Current-Density:
-(define-units/type Current-Density-Unit
-  )
+define-units/type Current-Density-Unit
+
 
 ;; Resistance:
-(define-units/type Resistance-Unit
+define-units/type Resistance-Unit
   [ohm (make-Unit 'ohm 1 resistance-dimension)]
-  )
+
 
 ;; Resistivity:
-(define-units/type Resistivity-Unit
+define-units/type Resistivity-Unit
   [ohm-meter (u* ohm meter)]
-  )
+
 
 ;; Conductivity:
-(define-units/type Conductivity-Unit
-  )
+define-units/type Conductivity-Unit
+
 
 ;; Magnetic-Field:
-(define-units/type Magnetic-Field-Unit
+define-units/type Magnetic-Field-Unit
   [tesla (make-Unit 'tesla 1 magnetic-field-dimension)]
   [gauss (u* 1e-4 tesla)]
-  )
+
 
 ;; Inductance:
-(define-units/type Inductance-Unit
+define-units/type Inductance-Unit
   [henry (make-Unit 'henry 1 inductance-dimension)]
-  )
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
